9dc80d6e6bd99d0e5cba9e74511b1d700a802028
VladimirRiz <resin.fhp@gmail.com>
2022-06-11 16:40:53 +0300

    feat: structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { BacklogComponent } from './backlog/backlog.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProjectsComponent,
+    BacklogComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule
